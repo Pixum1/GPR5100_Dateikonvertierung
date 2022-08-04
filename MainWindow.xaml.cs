de657048d2159330d008d2990a71c4a9fc02d9d6
Pixum <pixum2001@gmail.com>
2022-08-04 11:29:40 +0200

    Error prevention

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Text;
 using System.Windows;
+using Microsoft.Win32;
 
 namespace GPR5100_Dateikonvertierung
 {
@@ -11,53 +12,69 @@ namespace GPR5100_Dateikonvertierung
     public partial class MainWindow : Window
     {
         string selectedFilePath;
-        string fileFormat = "All Files|*.*";
+        const string FILE_FORMAT = "All Files|*.*";
 
         public MainWindow()
         {
             InitializeComponent();
         }
+        private void ConvertToTextFile(string _from, string _to)
+        {
+            string msg = Convert.ToBase64String(File.ReadAllBytes(_from));
+            File.WriteAllText(_to, msg);
+        }
+        private void ConvertFromTextFile(string _from, string _to)
+        {
+            byte[] data = Convert.FromBase64String(File.ReadAllText(_from));
+
+            using (FileStream fs = new FileStream(_to, FileMode.OpenOrCreate))
+            {
+                fs.Write(data, 0, data.Length);
+                fs.Close();
+            }
+        }
+        private void ApplyFilter(string _path, SaveFileDialog _filter)
+        {
+            if (Path.GetExtension(_path) == ".txt")
+                _filter.Filter = FILE_FORMAT;
+            else
+                _filter.Filter = "TXT Files|*.txt";
+        }
+
         private void OnClick_SaveAs(object sender, RoutedEventArgs e)
         {
             try
             {
-                Microsoft.Win32.SaveFileDialog saveDlg = new Microsoft.Win32.SaveFileDialog();
+                SaveFileDialog saveDlg = new SaveFileDialog();
 
-                if (Path.GetExtension(selectedFilePath) == ".txt")
-                    saveDlg.Filter = fileFormat;
-                else
-                    saveDlg.Filter = "TXT Files|*.txt";
+                ApplyFilter(selectedFilePath, saveDlg);
 
-                saveDlg.ShowDialog();
-
-                if (!Path.HasExtension(saveDlg.FileName))
+                if (saveDlg.ShowDialog() == true)
                 {
-                    ShowError("You have to choose a valid format!");
-                    return;
-                }
-                if (Path.GetExtension(saveDlg.FileName) == Path.GetExtension(selectedFilePath))
-                {
-                    ShowError("You can't convert to the same file format!");
-                    return;
-                }
-
-                if (Path.GetExtension(saveDlg.FileName) == ".txt")
-                {
-                    string msg = Convert.ToBase64String(File.ReadAllBytes(selectedFilePath));
-                    File.WriteAllText(saveDlg.FileName, msg);
-                }
-                else
-                {
-                    byte[] data = Convert.FromBase64String(File.ReadAllText(selectedFilePath));
-
-                    using (FileStream fs = new FileStream(saveDlg.FileName, FileMode.OpenOrCreate))
+                    if (!Path.HasExtension(saveDlg.FileName))
                     {
-                        fs.Write(data, 0, data.Length);
-                        fs.Close();
+                        ShowError("You have to choose a valid format!");
+                        return;
                     }
-                }
+                    if (Path.GetExtension(saveDlg.FileName) == Path.GetExtension(selectedFilePath))
+                    {
+                        ShowError("You can't convert to the same file format!");
+                        return;
+                    }
 
-                MessageBox.Show($"Succesfully converted from {Path.GetExtension(selectedFilePath)} to {Path.GetExtension(saveDlg.FileName)}");
+                    //Convert selected file to a new text file
+                    if (Path.GetExtension(saveDlg.FileName) == ".txt")
+                    {
+                        ConvertToTextFile(selectedFilePath, saveDlg.FileName);
+                    }
+                    //Convert selected file to a new format
+                    else
+                    {
+                        ConvertFromTextFile(selectedFilePath, saveDlg.FileName);
+                    }
+
+                    MessageBox.Show($"Succesfully converted from {Path.GetExtension(selectedFilePath)} to {Path.GetExtension(saveDlg.FileName)}");
+                }
             }
             catch (Exception _e)
             {
@@ -65,23 +82,20 @@ namespace GPR5100_Dateikonvertierung
             }
         }
 
-        byte[] GetStringData(string _string)
-        {
-            return Convert.FromBase64String(_string);
-        }
-
         private void OnClick_SelectFile(object sender, RoutedEventArgs e)
         {
             try
             {
-                Microsoft.Win32.OpenFileDialog openDlg = new Microsoft.Win32.OpenFileDialog();
-                openDlg.Filter = fileFormat;
-                openDlg.ShowDialog();
+                OpenFileDialog openDlg = new OpenFileDialog();
+                openDlg.Filter = FILE_FORMAT;
 
-                //if (!(System.IO.Path.GetExtension(openDlg.FileName).ToLower() == ".txt")) return;
+                if (openDlg.ShowDialog() == true)
+                {
+                    selectedFilePath = openDlg.FileName;
+                    Txt_SelectedPath.Text = selectedFilePath;
 
-                selectedFilePath = openDlg.FileName;
-                Txt_SelectedPath.Text = selectedFilePath;
+                    Btn_SaveAs.IsEnabled = true;
+                }
             }
             catch (Exception _e)
             {
